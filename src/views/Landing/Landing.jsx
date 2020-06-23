8cbf3cdbcc3276b2f2dfbbef6e398cdcdf3ef640
J. Zhang <jz2916@columbia.edu>
2020-06-23 10:47:33 -0400

    Getting started changing the Landing page.

--- src/views/Landing/Landing.jsx
+++ src/views/Landing/Landing.jsx
@@ -4,23 +4,36 @@ import {Divider, Layout} from 'antd';
 import "../../styles.css"
 
 import FAQ from "./Sections/FAQ";
-import Samples from "./Sections/Samples";
-import Carousel from "./Sections/Carousel";
+import Welcome from "./Sections/Welcome";
 import HowItWorks from "./Sections/HowItWorks";
+import Create from "./Sections/Create"
+import Register from "./Sections/Register"
+import Resources from "./Sections/Resources"
+
 import PageWrapper from "../../PageWrapper"
+function LandingDivider(){
+    return( <Divider style={{minWidth: "1em", marginLeft: "10%", width: "80%", height: "1.5px"}}/> )
+}
 
 class Landing extends React.Component {
     render() {
         return (
             <PageWrapper content={
-                <Layout.Content style={{background: "white"}}>
-                    <Carousel />
+                //<Layout.Content style={{background: "white"}}>
+                <div style={{background: "white"}}>
+                    <Welcome />
+                    <LandingDivider />
                     <HowItWorks />
-                    <Divider style={{minWidth: "1em", marginLeft: "10%", width: "80%", height: "1.5px"}}/>
-                    <Samples />
-                    <Divider style={{minWidth: "1em", marginLeft: "10%", width: "80%", height: "1.5px"}}/>
+                    <LandingDivider />
+                    <Create />
+                    <LandingDivider />
+                    <Register />
+                    <LandingDivider />
+                    <Resources />
+                    <LandingDivider />
                     <FAQ />
-                </Layout.Content>
+                </div>
+                //</Layout.Content>
             }/>
         );
     }
