8f76942b8649efadc84bc54e337037480f8ee78e
J. Zhang <jz2916@columbia.edu>
2020-06-24 14:58:54 -0400

    Continuing to consolidate our progress.

--- src/views/Landing/Sections/Welcome.jsx
+++ src/views/Landing/Sections/Welcome.jsx
@@ -30,55 +30,47 @@ export default class Carousel extends React.Component{
                     <Row className="justify-content-md-center">
                         <Col style={{ "paddingLeft": 0, "paddingRight": 0 }}>
                             <div className="parallax">
-                                    <Container>
-                                        <Row>
-                                            <Col>
-                                                <div className="header">We are sorry you are here, <br/> but happy you found us!</div>
-                                            </Col>
-                                        </Row>
-                                        <Row className="middle">
-                                            <Col sm="0" md="5">
-                                                <div ></div>
-                                            </Col>
-                                            <Col sm="12" md="7" className="line" className="vertical-line">
-                                                <Row>
-                                                    <Col>
-                                                        Tell their story.
-                                                    </Col>
-                                                </Row>
-                                                <Row >
-                                                    <Col>
-                                                        Fund their resting place.
-                                                    </Col>
-                                                </Row>
-                                                <Row>
-                                                    <Col>
-                                                        Commemorate and celebrate life with RestEasy.
-                                                    </Col>
-                                                </Row>
-                                            </Col>
-                                        </Row>
-                                        <Row className="button-row">
-                                            <Col sm="4">
-                                                <Button className="button-links" onClick={this.state.createScroll}>Create <br/> <span className="sub-btn">A Digital Memory</span></Button>
-                                            </Col>
-                                            <Col sm="4">
-                                                <Button className="button-links" onClick={this.state.registerScroll}>Register <br/> <span className="sub-btn">For What You Need</span></Button>
-                                            </Col >
-                                            <Col sm="4">
-                                                <Button className="button-links" onClick={this.state.resourcesScroll}>Find <br/> <span className="sub-btn">Advice and Resources</span></Button>
-                                            </Col>
-                                        
-                                        </Row>
-                                    
-                                    </Container>
-                                    
-                                </div>
-
-                            <div className="section">
-
+                                <Container>
+                                    <Row>
+                                        <Col>
+                                            <div className="header">We are sorry you are here, <br/> but happy you found us!</div>
+                                        </Col>
+                                    </Row>
+                                    <Row className="middle">
+                                        <Col sm="0" md="5">
+                                            <div ></div>
+                                        </Col>
+                                        <Col sm="12" md="7" className="line" className="vertical-line">
+                                            <Row>
+                                                <Col>
+                                                    Tell their story.
+                                                </Col>
+                                            </Row>
+                                            <Row >
+                                                <Col>
+                                                    Fund their resting place.
+                                                </Col>
+                                            </Row>
+                                            <Row>
+                                                <Col>
+                                                    Commemorate and celebrate life with RestEasy.
+                                                </Col>
+                                            </Row>
+                                        </Col>
+                                    </Row>
+                                    <Row className="button-row">
+                                        <Col sm="4">
+                                            <Button className="button-links" onClick={this.state.createScroll}>Create <br/> <span className="sub-btn">A Digital Memory</span></Button>
+                                        </Col>
+                                        <Col sm="4">
+                                            <Button className="button-links" onClick={this.state.registerScroll}>Register <br/> <span className="sub-btn">For What You Need</span></Button>
+                                        </Col >
+                                        <Col sm="4">
+                                            <Button className="button-links" onClick={this.state.resourcesScroll}>Find <br/> <span className="sub-btn">Advice and Resources</span></Button>
+                                        </Col>
+                                    </Row>
+                                </Container>
                             </div>
-                        
                         </Col>
                     </Row>
             </Container>
