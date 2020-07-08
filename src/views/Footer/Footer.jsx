70f4a0ad872a552a63c593a7e0f700dc1d2aead1
J. Zhang <jz2916@columbia.edu>
2020-07-08 14:26:00 -0400

    Made things a little more spaced out.

--- src/views/Footer/Footer.jsx
+++ src/views/Footer/Footer.jsx
@@ -5,14 +5,14 @@ import "../../styles/styles.css"
 export default function Footer() {
     return (
         <div style={{backgroundColor: "var(--Color1)"}}>
-                        <span style={{margin: "40px", color: "white"}}> © 2020 RestEasy Technologies, Inc. </span>
-                        <a href="/privacypolicy" style={{margin: "40px", color: "white"}}> Privacy Policy / Terms & Conditions </a>
-                        <a href="/about" style={{margin: "40px", color: "white"}}> About RestEasy </a>
-                        <a href="/press" style={{margin: "40px", color: "white"}}> Press </a>
-                        <img src={require('../../assets/img/facebook.png')} style={{margin: "10px", maxHeight: "40px"}} />
-                        <img src={require('../../assets/img/instagram.png')} style={{margin: "10px",maxHeight: "40px"}} />
-                        <img src={require('../../assets/img/linkedin.png')} style={{margin: "10px",maxHeight: "40px"}} />
-                        <a href="/" style={{margin: "40px", color: "white"}}> #resteasier </a>
+            <span style={{margin: "50px", color: "white"}}> © 2020 RestEasy Technologies, Inc. </span>
+            <a href="/privacypolicy" style={{margin: "50px", color: "white"}}> Privacy Policy / Terms & Conditions </a>
+            <a href="/about" style={{margin: "50px", color: "white"}}> About RestEasy </a>
+            <a href="/press" style={{margin: "50px", color: "white"}}> Press </a>
+            <img src={require('../../assets/img/facebook.png')} style={{margin: "10px", maxHeight: "40px"}} />
+            <img src={require('../../assets/img/instagram.png')} style={{margin: "10px",maxHeight: "40px"}} />
+            <img src={require('../../assets/img/linkedin.png')} style={{margin: "10px",maxHeight: "40px"}} />
+            <a href="/" style={{margin: "50px", color: "white"}}> #resteasier </a>
             <br />
         </div>
     )
