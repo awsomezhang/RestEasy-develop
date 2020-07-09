96f3d7a7921cc4b57832003f41765e97d01b00c3
coleman <scoleman@simchas-mbp.myfiosgateway.com>
2020-07-09 12:27:27 -0400

    update logo. refine header. remove transparency on scroll

--- src/views/Header/Header.jsx
+++ src/views/Header/Header.jsx
@@ -1,49 +1,55 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import "./Header.css"
 import {Col, Nav, Navbar, Container, NavDropdown, Form, FormControl, Button, InputGroup} from "react-bootstrap"
 import { withRouter } from "react-router-dom";
+import { Input } from 'antd';
 
+const { Search } = Input;
 
 function Header({ location }){
     const { pathname } = location;
 
+    const [scroll, setScroll] = useState(true)
+
+    useEffect(() => {
+        document.addEventListener("scroll", () => {
+            const scrollCheck = window.scrollY < 100
+            if (scrollCheck !== scroll) {
+                setScroll(scrollCheck)
+            }
+        })
+        console.log(scroll)
+    })
+
     return (
         <div className="header">
-            <Navbar className="navBar-custom" fixed="top" expand="lg">
+            <Navbar className={scroll ?  null : "navBar-custom-dark"} fixed="top" expand="lg">
                 <Navbar.Brand href="/">
                     <img
-                        src={require('../../assets/img/logo.png')}
-                        width="50"
-                        height="50"
+                        src={require('../../assets/img/Logo.png')}
+                        width="99px"
+                        height="79px"
                         className="d-inline-block align-top"
-                        alt="RestEasy logo"
+                        alt="RestEasylogo"
                     />
                 </Navbar.Brand>
-                <span onClick={e => window.location.href = '/'} className="title">RestEasy</span>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav" className="menu-options">
                     <Nav className="mr-auto">
-                        <Nav.Link href="/about">About</Nav.Link>
-                        <Nav.Link href="">FAQ</Nav.Link>
-                        <Nav.Link href="/whattodonow">What to do Now?</Nav.Link>
-                        <Nav.Link href="">Help a friend in need</Nav.Link>
-                        <Nav.Link href="/registry">Registry</Nav.Link>
-                        <Nav.Link href="/my/create">Create</Nav.Link>
-                        <Nav.Link href="/contact">Contact Us!</Nav.Link>
+                        <Nav.Link href="/about"><span className="header-link">About</span></Nav.Link>
+                        <Nav.Link href=""><span className="header-link">FAQ</span></Nav.Link>
+                        <Nav.Link href="/whattodonow"><span className="header-link">What to do Now?</span></Nav.Link>
+                        <Nav.Link href=""><span className="header-link">Help a friend in need</span></Nav.Link>
+                        <Nav.Link href="/registry"><span className="header-link">Registry</span></Nav.Link>
+                        <Nav.Link href="/my/create"><span className="header-link">Create</span></Nav.Link>
+                        <Nav.Link href="/contact"><span className="header-link">Contact Us!</span></Nav.Link>
                     </Nav>
-                    <div className="search-bar">
+                    <div className="search-area">
                         <Form inline>
-                            <InputGroup>
-                                <FormControl
+                            <FormControl className="search-bar"
                                     type="text"
                                     placeholder="Search"
-                                    aria-label="Input group example"
-                                    aria-describedby="btnGroupAddon"
-                                />
-                                <InputGroup.Prepend>
-                                    <Button id="btnGroupAddon" variant="outline-success">Search</Button>
-                                </InputGroup.Prepend>
-                            </InputGroup>
+                            />
                         </Form>
                     </div>
                     
