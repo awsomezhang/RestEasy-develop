1dd96c6a65f633ff8a46be8110217571b42b687b
coleman <scoleman@simchas-mbp.myfiosgateway.com>
2020-07-13 17:05:57 -0400

    add backend for login, register, and validated user session

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const cors = require('cors')
+require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('./_helpers/jwt');
+const errorHandler = require('./_helpers/error-handler');
 
 // Setup express app
-const app = express();
 app.use(express.json());
-
 
 app.use(
     bodyParser.urlencoded({
@@ -16,33 +17,31 @@ app.use(
 
 app.use(bodyParser.json());
 app.use(cors())
-app.use((req, res, next) => {
-    res.send('Welcome to Express');
-  });
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-next();
-});
+// use JWT auth to secure the api
+app.use(jwt());
 
+// api routes
+app.use('/users', require('./users/users.controller'));
+
+app.use(errorHandler);
 
 // Configure Mongo
 
 // loading my personal URI for testing purposes. Keep file private (.gitignore)
-var mongoCreds = require('./mongoCreds.json');
+// var mongoCreds = require('./mongoCreds.json');
 
-mongoose.connect(
-    mongoCreds.URI, { 
-        useNewUrlParser: true, 
-        useCreateIndex: true 
-    }
-);
+// mongoose.connect(
+//     mongoCreds.URI, { 
+//         useNewUrlParser: true, 
+//         useCreateIndex: true 
+//     }
+// );
 
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
-})
+// const connection = mongoose.connection;
+// connection.once('open', () => {
+//   console.log("MongoDB database connection established successfully");
+// })
 
 // Specify the Port where the backend server can be accessed and start listening on that port
 const port = process.env.PORT || 5000;
